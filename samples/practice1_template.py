f806af5edbecd549109147c96229b92660dd7a6e
tma <tma@S.local>
2019-11-19 17:37:32 +0300

    commitone

--- samples/practice1_template.py
+++ samples/practice1_template.py
@@ -2,12 +2,15 @@ import sys
 import cv2
 import logging as log
 import argparse
+import array
 
 sys.path.append('../src')
 from imagefilter import ImageFilter
 
 def build_argparse():
     parser=argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help = 'your cmd argument', type = str)
+    parser.add_argument('-g', '--isGrayscale', help = 'your cmd argument', type = str)
     
     #
     # Add your code here
@@ -20,11 +23,21 @@ def main():
     log.info("Hello image filtering")
     args = build_argparse().parse_args()
     
-    #
-    # Add your code here
-    #
+    variable = args.input
+    isGrayscale = args.isGrayscale
+    log.info('nanana')
+    
+    image = cv2.imread(variable)
+    if(isGrayscale)
+        cvtColor(image,image,COLOR_RGBA2GRAY)
+    width = image.shape[0]
+    image[ : width //2 , :, :] = 0;
+    image=cv2.resize(image, dsize  = (512,512))
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
     return
     
-if __name__ == '__main__':
+if __name__ == '__main__':         
     sys.exit(main())
